75ea061cc0458826581405c570bfc20cbf019c4d
B. Roux <B-Roux@users.noreply.github.com>
2022-10-23 21:06:26 -0500

    Style updates

--- script.js
+++ script.js
@@ -1,41 +1,94 @@
-makeSubredditPage(
-    "https://www.reddit.com/r/pics/.json",
-    document.querySelector("#destination")
-);
+// Main
+window.addEventListener('DOMContentLoaded', (event) => {
+    getNewPostPage();
+});
+
+// UI controls
+subredditToFetch = "all";
+function changeSubreddit(value) {
+    subredditToFetch = value;
+}
+
+sortToFetch = "hot";
+function changeSort(value) {
+    if (value == "controversial" || value == "top") {
+        document.querySelector("#from-time-radio").style = "display: initial;";
+    } else {
+        document.querySelector("#from-time-radio").style = "display: none;";
+    }
+    sortToFetch = value;
+}
+
+timeToFetch = "all";
+function changeTime(value) {
+    timeToFetch = value;
+}
+
+function makeRedditURL() {
+    let url = "https://api.reddit.com/r/" + subredditToFetch + "/" + sortToFetch + "/.json";
+    if (sortToFetch == "controversial" || sortToFetch == "top") {
+        url += "?t=" + timeToFetch;
+    }
+    return url;
+}
+
+function getNewPostPage() {
+    url = makeRedditURL();
+    console.log(url);
+    makeSubredditPage(
+        url,
+        document.querySelector(".post-list")
+    );
+}
+
+// Reddit API interface
+
 
 function makeSubredditPage(url, destination) {
-    fetchRedditPage(url).then(function (data) {
-        let children = data.data.children;
-        children.forEach(child => { destination.appendChild(makePostNode(child)); });
-    });
+    fetchRedditPage(url)
+        .then(function (data) {
+            destination.replaceChildren([]);
+            data.data.children.forEach(child => { destination.appendChild(makePostNode(child)); });
+        })
+        .catch(function (err) {
+            alert(err);
+        });
 }
 
 function makePostNode(post) {
     const redditURL = "https://www.reddit.com/";
 
-    let cont = document.createElement("div");
-    cont.setAttribute("class", "post-container");
+    let container = document.createElement("div");
+    container.setAttribute("class", "post-container");
+
+    let left = document.createElement("div");
+    left.setAttribute("class", "post-container-left");
+
+    let right = document.createElement("div");
+    right.setAttribute("class", "post-container-right");
 
     let score = document.createElement("div");
     score.setAttribute("class", "post-score");
     score.appendChild(document.createTextNode(post.data.score.toLocaleString()));
-    cont.appendChild(score);
+    left.appendChild(score);
 
     if (
         post.data.thumbnail == "self"
         || post.data.thumbnail == "spoiler"
         || post.data.thumbnail == "nsfw"
+        || post.data.thumbnail == "default"
+        || post.data.thumbnail == ""
     ) {
         let thumbnailContainer = document.createElement("div");
-        thumbnailContainer.setAttribute("class", "post-thumbnail");
-        cont.appendChild(thumbnailContainer);
+        thumbnailContainer.setAttribute("class", "post-no-thumbnail no-thumbnail-type-" + post.data.thumbnail);
+        left.appendChild(thumbnailContainer);
     } else {
         let thumbnailContainer = document.createElement("div");
         thumbnailContainer.setAttribute("class", "post-thumbnail");
         let thumbnail = document.createElement("img");
         thumbnail.setAttribute("src", post.data.thumbnail);
         thumbnailContainer.appendChild(thumbnail);
-        cont.appendChild(thumbnailContainer);
+        left.appendChild(thumbnailContainer);
     }
 
     let title = document.createElement("div");
@@ -45,7 +98,7 @@ function makePostNode(post) {
         title.setAttribute("class", "post-title");
     }
     title.appendChild(document.createTextNode(post.data.title));
-    cont.appendChild(title);
+    right.appendChild(title);
 
     let byline = document.createElement("div");
     byline.setAttribute("class", "post-byline");
@@ -61,21 +114,26 @@ function makePostNode(post) {
     subreddit.setAttribute("href", redditURL + "r/" + post.data.subreddit);
     subreddit.appendChild(document.createTextNode(post.data.subreddit));
     byline.appendChild(subreddit);
+    let millisSincePosted = Math.round(Date.now() - post.data.created*1000);
+    byline.appendChild(document.createTextNode(" " + formatDuration(millisSincePosted) + " ago"));
+    if (post.data.edited) {
+        byline.appendChild(document.createTextNode("*"));
+    }
     if (post.data.over_18) {
         let nsfwTag = document.createElement("span");
         nsfwTag.setAttribute("class", "post-nsfw-tag");
-        nsfwTag.appendChild(document.createTextNode("nsfw"));
+        nsfwTag.appendChild(document.createTextNode("(nsfw)"));
         byline.appendChild(document.createTextNode(" "));
         byline.appendChild(nsfwTag);
     }
     if (post.data.spoiler) {
         let spoilerTag = document.createElement("span");
         spoilerTag.setAttribute("class", "post-spoiler-tag");
-        spoilerTag.appendChild(document.createTextNode("spoiler"));
+        spoilerTag.appendChild(document.createTextNode("(spoiler)"));
         byline.appendChild(document.createTextNode(" "));
         byline.appendChild(spoilerTag);
     }
-    cont.appendChild(byline);
+    right.appendChild(byline);
 
     let actions = document.createElement("div");
     actions.setAttribute("class", "post-actions");
@@ -90,10 +148,28 @@ function makePostNode(post) {
     openOnReddit.setAttribute("href", redditURL + post.data.permalink);
     openOnReddit.setAttribute("target", "_blank");
     actions.appendChild(openOnReddit);
-    cont.appendChild(actions);
+    right.appendChild(actions);
 
+    container.appendChild(left);
+    container.appendChild(right);
+    return container;
+}
 
-    return cont;
+function formatDuration(millis) {
+    let seconds = millis/1000;
+    if (seconds < 60) {
+        return Math.round(seconds).toString() + " seconds";
+    }
+    let minutes = seconds/60;
+    if (minutes < 60) {
+        return Math.round(minutes).toString() + " minutes";
+    }
+    let hours = minutes/60;
+    if (hours < 24) {
+        return Math.round(hours).toString() + " hours";
+    }
+    let days = hours/24;
+    return Math.round(days).toString() + " days";
 }
 
 function fetchRedditPage(url) {
